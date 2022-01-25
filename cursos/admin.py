935321172e2007f992e096de85383b13c16cc04c
wallace-braga <crs.pedroh@gmail.com>
2022-01-25 12:24:17 -0300

    first commit

---
+++ cursos/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Curso, Availiacao
+
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'url', 'criacao', 'atualizacao', 'ativo')
+
+
+
+@admin.register(Availiacao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ('curso', 'nome', 'email', 'avaliacao', 'criacao', 'atualizacao', 'ativo')

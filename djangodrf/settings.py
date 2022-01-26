c4ee7cff9bf1ee76baa7ac65dcb2a46502c6f43c
Pedro Henrique <84346634+Pedro-CRS@users.noreply.github.com>
2022-01-26 08:32:06 -0300

    Add files via upload

--- djangodrf/settings.py
+++ djangodrf/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8c1+1l-y^vrx5c90vu&%*#*plh#as86fa_wfjsrf#vu62g4gy^'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
